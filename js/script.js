a3d950a328b4b2c26645f187a42ec19beffb0755
jersonamaya <lopezjerson1798@gmail.com>
2023-07-13 11:17:42 -0600

    se modifico el archivo go

--- js/script.js
+++ js/script.js
@@ -12,3 +12,35 @@ loginLink.addEventListener('click',() =>{
     logregBox.classList.remove('active');
     
     });
+
+
+    // Función para enviar una imagen al servidor
+function enviarImagen(imageFile) {
+    const url = "http://localhost:8080/detect"; // Actualiza con la URL correcta del backend
+    
+    const formData = new FormData();
+    formData.append("image", imageFile);
+  
+    return fetch(url, {
+      method: "POST",
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Aquí puedes procesar los resultados recibidos del backend
+      console.log("Resultados:", data);
+    })
+    .catch(error => {
+      console.error("Error al enviar la imagen:", error);
+    });
+  }
+  
+  // Manejar el evento de cambio del input de archivos
+  const inputFile = document.querySelector("input[type='file']");
+  inputFile.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      enviarImagen(file);
+    }
+  });
+  
